645581fe4075a87e9eec118b78dbb519983c2209
Masterchef365 <duncan.freeman1@gmail.com>
2022-01-18 03:12:07 -0800

    Better solution, I think

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,25 @@ fn letter_idx(c: char) -> usize {
 }
 
 fn calc_optimal_first_word(dict: &[Word]) -> Word {
-    let mut letter_hist = [0u64; 26];
+    let mut letter_hists = [[0u64; 26]; 5];
 
     for word in dict {
-        for letter in word {
+        for (slot, letter) in word.iter().enumerate() {
             if ('A'..='Z').contains(letter) {
-                letter_hist[letter_idx(*letter)] += 1;
+                letter_hists[slot][letter_idx(*letter)] += 1;
             } else {
                 panic!("Wrong letter {}", letter);
             }
         }
     }
+
+    /*
+    let mut k: Vec<(usize, u64)> = letter_hist.iter().copied().enumerate().collect();
+    k.sort_by_key(|(_, count)| *count);
+    for (idx, count) in k {
+        dbg!((char::from('A' as u8 + idx as u8), count));
+    }
+    */
 
     let mut best_word = dict[0];
     let mut best_score = 0;
@@ -33,13 +41,13 @@ fn calc_optimal_first_word(dict: &[Word]) -> Word {
         let mut letter_used = [false; 26];
         let mut repeats = 1;
         let mut total = 0;
-        for letter in word {
+        for (slot, letter) in word.iter().enumerate() {
             let idx = letter_idx(*letter);
             if letter_used[idx] {
                 repeats += 1;
             }
 
-            total += letter_hist[idx];
+            total += letter_hists[slot][idx];
 
             letter_used[idx] = true;
         }
